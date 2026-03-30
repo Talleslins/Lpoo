df4ed498a76d25cf168c2fc802f728f5057d3eb9
Talleslins <pinheirotallesl@gmail.com>
2026-03-30 17:38:42 -0300

    Criação de algumas classes e alteração do README

---
+++ src/Player.java
@@ -0,0 +1,16 @@
+public class Player {
+    private String Nome;
+    private int Vida;
+    private int Pontuacao;
+    private Character PersonagemEscolhido;
+
+    public Player() {
+        this.Nome = nome;
+        this.Vida = vida;
+        this.Pontuacao = pontuacao;
+        this.PersonagemEscolhido = personagemEscolhido;
+
+    }
+
+
+}
