94de2bc30bd259de2eaa499b5198f6caf481c7b4
Diogo Bezerra <ddiogo.bezerraa@gmail.com>
2026-04-23 21:36:48 -0300

    Funcionamento da classe Question.java (em processo)

--- src/Question.java
+++ src/Question.java
@@ -1,5 +1,20 @@
 public abstract class Question {
-    private String Enunciado;
-    private int Dificuldade;
+    protected String Enunciado;
+    protected String Dificuldade;
+    protected String Resposta;
+
+    public Question(String Enunciado, String Dificuldade, String Resposta) {
+        this.Enunciado = Enunciado;
+        this.Dificuldade = Dificuldade;
+        this.Resposta = Resposta;
+    }
+
+    void setenunciado(String e) { Enunciado = e; }
+    void setdificuldade(String d) { Dificuldade = d; }
+    void setresposta(String r) { Resposta = r; }
+
+    abstract String exibirPergunta();
+
+    abstract boolean validarResposta(String r);
 
 }
