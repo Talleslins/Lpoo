8f1e062190bd1c19837077964dbbd8fb7e5ed4b3
Diogo Bezerra <ddiogo.bezerraa@gmail.com>
2026-04-23 21:38:05 -0300

    MultipleChoice.java (em processo)

--- src/MultipleChoice.java
+++ src/MultipleChoice.java
@@ -1,2 +1,16 @@
 public class MultipleChoice extends Question {
+  protected String Opcoes;
+
+    public MultipleChoice(String Opcoes) {
+        this.Opcoes = Opcoes;
+    }
+
+    void setopcoes(String o) { Opcoes = o; }
+
+    @Override
+    String exibirPergunta() {
+        System.out.println("--------------");
+        System.out.println(this.Enunciado);
+
+    }
 }
