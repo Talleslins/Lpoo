4322ff263ff685352311462f4c57a228422395bc
Diogo Bezerra <ddiogo.bezerraa@gmail.com>
2026-04-25 14:45:28 -0300

    Adição de métodos da classe TrueFalse

--- src/TrueFalse.java
+++ src/TrueFalse.java
@@ -1,15 +1,12 @@
-public class TrueFalse extends Question{
+public class TrueFalse extends Question {
     public TrueFalse() {
-        super("", "", "");
+        super("","","");
     }
 
     @Override
-    String exibirPergunta() {
-        return "";
-    }
-
-    @Override
-    boolean validarResposta(String r) {
-        return false;
+    void exibirPergunta() {
+        System.out.println("--------------");
+        System.out.println(this.Enunciado+"\n");
+        System.out.print("Informe se o enunciado é verdadeiro ou falso [v/f]: ");
     }
 }
