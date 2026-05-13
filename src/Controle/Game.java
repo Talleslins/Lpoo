543f15bccf50bba92bb687a05ab3a5dd4ff70825
Diogo Bezerra <ddiogo.bezerraa@gmail.com>
2026-05-13 03:05:27 -0300

    Game.java

--- src/Controle/Game.java
+++ src/Controle/Game.java
@@ -1,4 +1,32 @@
 package Controle;
 
+import Personagem.Cientista;
+import Personagem.Enemy;
+import Personagem.Player;
+import Questões.QuestionBank;
+import java.util.Scanner;
+
 public class Game {
+    private Player player;
+    private BattleManager battleManager; // [cite: 41]
+
+    public void iniciarJogo() { // [cite: 44]
+        Scanner scanner = new Scanner(System.in);
+        this.player = new Player("Talles", 0, new Cientista());
+        Enemy inimigo = new Enemy();
+        
+        QuestionBank banco = new QuestionBank();
+        banco.lerArquivo();
+        
+        this.battleManager = new BattleManager(player, inimigo);
+        
+        System.out.println("Bem-vindo ao Character Battle!");
+        boolean vitoria = battleManager.iniciarCombate(banco.getPerguntas(), scanner);
+        
+        if (vitoria) {
+            System.out.println("Vitória Geral!");
+        } else {
+            System.out.println("Game Over!");
+        }
+    }
 }
