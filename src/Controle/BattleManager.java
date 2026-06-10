e208d8d540ae66ffe5e8fbbabb09948e5b0c4c58
Diogo Bezerra <ddiogo.bezerraa@gmail.com>
2026-06-10 02:01:45 -0300

    Update BattleManager.java

--- src/Controle/BattleManager.java
+++ src/Controle/BattleManager.java
@@ -2,7 +2,9 @@ package Controle;
 
 import Personagem.Enemy;
 import Personagem.Player;
+import Personagem.Cientista;
 import Questões.Question;
+import Questões.MultipleChoice;
 
 import java.util.ArrayList;
 import java.util.Scanner;
@@ -20,12 +22,29 @@ public class BattleManager {
         int vidaAtualPlayer = jogador.getPersonagemEscolhido().getVida();
         int vidaAtualEnemy = inimigo.getVida();
 
+        if (jogador.getPersonagemEscolhido() instanceof Cientista) {
+            ((Cientista) jogador.getPersonagemEscolhido()).resetarHabilidade();
+        }
+
         while(vidaAtualPlayer > 0 && vidaAtualEnemy > 0 && !questoes.isEmpty()){
 
             System.out.println("----NOVO TURNO----");
             System.out.println("Sua vida: " + vidaAtualPlayer);
             System.out.println("Vida do inimigo: " + vidaAtualEnemy);
             Question perguntaAtual = questoes.remove(0);
+
+            if (jogador.getPersonagemEscolhido() instanceof Cientista && perguntaAtual instanceof MultipleChoice) {
+                Cientista cientista = (Cientista) jogador.getPersonagemEscolhido();
+                if (cientista.isHabilidadeDisponivel()) {
+                    System.out.println("\n[HABILIDADE] Você pode usar o Intelecto para eliminar 1 alternativa incorreta.");
+                    System.out.print("Deseja ativar agora? (s/n): ");
+                    String opcao = scanner.nextLine();
+                    if (opcao.equalsIgnoreCase("s")) {
+                        cientista.ativarHabilidade(perguntaAtual);
+                    }
+                }
+            }
+
             perguntaAtual.exibirPergunta();
             String resposta = scanner.nextLine();
 
